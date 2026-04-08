33a51c5052236e865166bf88d8af39025ccccff4
Vedanthdamn <vedanthdama@gmail.com>
2026-04-08 21:23:22 +0530

    refactor: move split server model to src.models

--- src/models/server_model.py
+++ src/models/server_model.py
@@ -18,12 +18,14 @@ def get_server_model(activation_dim: int = 32) -> tf.keras.Model:
         raise ValueError("activation_dim must be a positive integer.")
 
     inputs = tf.keras.Input(shape=(activation_dim,), name="server_input")
-    x = tf.keras.layers.Dense(16, activation="relu", name="server_dense_16")(inputs)
+    x = tf.keras.layers.Dense(16, activation="relu", kernel_initializer="he_normal", name="server_dense_16")(inputs)
+    x = tf.keras.layers.BatchNormalization(name="server_bn_16")(x)
+    x = tf.keras.layers.Dropout(0.2, name="server_dropout_16")(x)
     outputs = tf.keras.layers.Dense(1, activation="sigmoid", name="server_output")(x)
 
     model = tf.keras.Model(inputs=inputs, outputs=outputs, name="server_model")
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(),
+        optimizer=tf.keras.optimizers.Adam(learning_rate=2e-4, clipnorm=1.0),
         loss="binary_crossentropy",
         metrics=["accuracy"],
     )
